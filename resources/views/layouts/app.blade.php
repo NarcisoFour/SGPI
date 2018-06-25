5d06b9554637eb1acf4b01bab35a8c259322564c
NarcisoFour <NarcisoFour@gmail.com>
2018-06-24 22:08:37 -0600

    Rutas para Usuarios y Roles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">{{__('Users')}}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('roles.index') }}">{{__('Roles')}}</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +18,32 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    //Roles
+    Route::get('roles', 'RoleController@index')->name('roles.index')
+        ->middleware('permission:roles.index');
+    Route::get('roles/create', 'RoleController@create')->name('roles.create')
+        ->middleware('permission:roles.create');
+    Route::post('roles/store', 'RoleController@store')->name('roles.store')
+        ->middleware('permission:roles.create');
+    Route::get('roles/{role}', 'RoleController@show')->name('roles.show')
+        ->middleware('permission:roles.show');
+    Route::get('roles/{role}/edit', 'RoleController@edit')->name('roles.edit')
+        ->middleware('permission:roles.edit');
+    Route::put('roles/{role}', 'RoleController@update')->name('roles.update')
+        ->middleware('permission:roles.edit');    
+    Route::delete('roles/{role}', 'RoleController@destroy')->name('roles.destroy')
+        ->middleware('permission:roles.destroy');
+    //Users
+    Route::get('users', 'UserController@index')->name('users.index')
+        ->middleware('permission:users.index');
+    Route::get('users/{user}', 'UserController@show')->name('users.show')
+        ->middleware('permission:users.show');
+    Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit')
+        ->middleware('permission:users.edit');
+    Route::put('users/{user}', 'UserController@update')->name('users.update')
+        ->middleware('permission:users.edit');
+    Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy')
+        ->middleware('permission:users.destroy');    
+});
